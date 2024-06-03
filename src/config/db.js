181ab883b0818e9d7bf195349f227027adb9c7a4
darabperwaiz <darabperwaiz87@gmail.com>
2024-06-03 21:12:26 +0530

    Ready to deploy

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const uri = process.env.DB_URI
 
 export const db = () => {
-    mongoose.connect(uri).then(()=> {
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=> {
         console.log("Database Connected Successfully!")
     })
 }
